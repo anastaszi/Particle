7b943287879193cd09ca4b1ee73fcc535050ab21
anastaszi <anastasba@gmail.com>
2020-09-07 16:48:19 -0700

    adding temp

--- purple_air_monitor.cpp
+++ purple_air_monitor.cpp
@@ -1,3 +1,4 @@
+/////////////////////////////////////////////
 #include "application.h"
 
 // This #include statement was automatically added by the Particle IDE.
@@ -15,11 +16,48 @@ http_header_t headers[] = {
 };
 http_request_t request;
 http_response_t response;
+int previous_aqi;
 
 struct AQI_result {
   String quality;
   int aqi;
+  int temp;
 } AQI_result;
+
+
+byte arrow_up[] = {
+  0x04,
+  0x0E,
+  0x15,
+  0x04,
+  0x04,
+  0x04,
+  0x04,
+  0x04
+};
+
+byte arrow_down[] = {
+  0x04,
+  0x04,
+  0x04,
+  0x04,
+  0x04,
+  0x15,
+  0x0E,
+  0x04
+};
+
+byte same[] = {
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x0E,
+  0x00,
+  0x00,
+  0x00
+};
+
 
 int converter(double value, double lower_bound, double upper_bound, int floor, int ceiling) {
   int result = (int)((value - lower_bound) * (ceiling - floor)/(upper_bound - lower_bound)) + floor;
@@ -30,6 +68,7 @@ struct AQI_result pm_to_aqi(const char *str) {
   int i = 0, array_len = 8;
   struct AQI_result result;
   double pm_value = atof(str);
+
   // array of pm and aqi boundaries
   double pm_b[8][2]  = {{0, 12.0}, {12.1, 35.4}, {35.5, 55.4}, {55.5, 150.4}, {150.5, 250.4
 }, {250.5, 350.4}, {350.5, 500.4}, {500.5, 99999.9}};
@@ -42,6 +81,7 @@ struct AQI_result pm_to_aqi(const char *str) {
       break;
     }
   }
+
   return result;
 }
 
@@ -53,10 +93,7 @@ struct AQI_result pm_to_aqi(const char *str) {
 LiquidCrystal lcd(D0, D1, D2, D3, D4, D5);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16,2);
-  // Print a first string to the LCD.
-  lcd.print("AQI in Los Altos");
+    previous_aqi = 0;
 }
 
 void loop() {
@@ -73,25 +110,69 @@ void loop() {
     strcpy(result, response.body.c_str());
 
     // Parse response json and get value of PM2_5
-    const size_t capacity = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(29) + JSON_OBJECT_SIZE(40) + 1320;
+    size_t capacity = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(28) + JSON_OBJECT_SIZE(39);
     DynamicJsonDocument doc(capacity);
 
-    const char* json = result;
     deserializeJson(doc, result);
     JsonObject results_0 = doc["results"][0];
     const char* results_0_PM2_5Value = results_0["PM2_5Value"];
     struct AQI_result aqi_res = pm_to_aqi(results_0_PM2_5Value);
-    char buf[20];
-    itoa(aqi_res.aqi, buf, 10);
 
-    Particle.publish(results_0_PM2_5Value,buf,aqi_res.aqi,PRIVATE);
+    Particle.publish("PM_AQI",results_0_PM2_5Value,aqi_res.aqi,PRIVATE);
+
+
+
+
+
+
+
+
+    // second request
+    request.hostname = "api.openweathermap.org";
+    request.port = 80;
+    request.path =  "/data/2.5/weather?id=5368335&units=imperial&APPID=ADD_YOUR_ID";
+    // Get request
+    http.get(request, response, headers);
+
+    capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(13) + 420;
+
+    DynamicJsonDocument doc2(capacity);
+
+    size_t size2 = response.body.length();
+    char result2[size2 + 1];
+    strcpy(result2, response.body.c_str());
+
+    deserializeJson(doc2, result2);
+
+     //result.temp = (temp_f − 32)*5.0/9;
+    float main_temp =doc2["main"]["temp"];
+    aqi_res.temp = (main_temp - 32) * 5.0 / 9;
+    lcd.clear();
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16,2);
+    // Print a first string to the LCD.
+    lcd.print("Temp: ");
+    lcd.print(aqi_res.temp);
+    lcd.print((char)223);
+    lcd.print("C");
 
     // set the cursor to column 0, line 1
     // (note: line 1 is the second row, since counting begins with 0):
-    lcd.setCursor(0, 1);
+
     // print the number of seconds since reset:
+    if (previous_aqi < aqi_res.aqi)
+        lcd.createChar(0, arrow_up);
+    else if (previous_aqi > aqi_res.aqi)
+        lcd.createChar(0, arrow_down);
+    else
+        lcd.createChar(0, same);
+    lcd.setCursor(0, 1);
+    lcd.print("Aqi: ");
     lcd.print(aqi_res.aqi);
+    lcd.write(0);
     lcd.print(" ");
     lcd.print(aqi_res.quality);
+    previous_aqi = aqi_res.aqi;
+    delay(5000);
     delay(150000);
 }
